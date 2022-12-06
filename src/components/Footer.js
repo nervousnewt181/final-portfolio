be7fe68fedbb2562d0182ebab6efdf0ea94644e7
nervousnewt181 <114962692+nervousnewt181@users.noreply.github.com>
2022-12-06 16:35:57 -0500

    Delete src directory

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-    return (
-        <div className='footer'>
-            <p className='footer-text'>© Nervous Newt 2022</p>
-        </div>
-    )
-}
